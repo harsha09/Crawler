cbeac9aa9e0f2ee988b674e3d77bf42b18a0cbbc
Harsha Bharadwaj <harsha.bajju@gmail.com>
2018-04-01 18:35:44 +0530

    ENH: Return type and attributes.

--- crawler/WebCrawler.py
+++ crawler/WebCrawler.py
@@ -6,42 +6,8 @@ from user_agent import generate_user_agent
 class WebCrawler(object):
     """Crawls web page for the url provided.
     args: header, url, selectors.
-    optional args: header"""
-
-    def __init__(self, **kwargs):
-
-        header = {'User-Agent': generate_user_agent(device_type='desktop', os=('linux', 'mac'))}
-        self.header = kwargs.get('header', header)
-        self.url = kwargs.get('url')
-        self.selectors = kwargs.get('selectors')
-
-        if self.url is None:
-            raise KeyError('Give a url to scrape.')
-
-        if self.selectors is None:
-            raise KeyError('Provide Selectors to extract the content.')
-
-        if not isinstance(self.selectors, dict):
-            raise ValueError('Selectors should be of type key value pairs.')
-
-    @classmethod
-    def get_text(cls, args):
-        return [arg.text for arg in args]
-    
-    def _response(self):
-        response = get(self.url, headers=self.header, timeout=10)
-        if response.status_code == 200:
-            return BeautifulSoup(response.text, 'lxml')
-        else:
-            response.raise_for_status()
-
-    def crawl(self):
-        output = {}
-        for key, value in self.selectors.items():
-            output[key] = ". ".join([dom.text.strip() for dom in self._response().select(value)])
-        return output
-
-if __name__ == '__main__':
+    optional args: header
+    Example:
     kwargs = {
     'url': 'https://www.fnlondon.com/articles/uk-seeks-to-soothe-fund-managers-delegation-fears-20180329',
     'selectors': {
@@ -50,5 +16,70 @@ if __name__ == '__main__':
     'date': '.op-modified'
     }
     }
-    print(WebCrawler(**kwargs).crawl())
+    kwargs = {
+    'url': 'https://www.bloomberg.com/markets',
+    'selectors':{
+        'stories_link': {
+            'selector': '.top-news-v3__stories .top-news-v3-story-view .top-news-v3-story-headline a',
+            'return_type': 'list',
+            'attr': 'href'
+            }
+        }
+    }
+    print(WebCrawler(**kwargs).crawl())    
+    """
+
+    def __init__(self, **kwargs):
+
+        header = {'User-Agent': generate_user_agent(device_type='desktop', os=('linux', 'mac'))}
+        self.header = kwargs.get('header', header)
+        self.url = kwargs.get('url')
+        self.selectors = kwargs.get('selectors')
+
+        if not self.url:
+            raise KeyError('Give a url to scrape.')
+
+        if self.selectors is None:
+            raise KeyError('Provide Selectors to extract the content.')
+
+        if not isinstance(self.selectors, dict):
+            raise ValueError('Selectors should be of type key value pairs.')
+    
+    def _response(self):
+        response = get(self.url, headers=self.header, timeout=10)
+        if response.status_code == 200:
+            return BeautifulSoup(response.text, 'lxml')
+        else:
+            response.raise_for_status()
+
+    @classmethod
+    def get_output(cls, scraped, return_type, attr):
+
+        output = [dom[attr].strip() for dom in scraped] if attr else [dom.text.strip() for dom in scraped]
+
+        if return_type == 'string':
+            output = '. '.join(output)
+
+        return output
+
+
+    def crawl(self):
+        output = {}
+        selector = ''
+        return_type = 'string'
+        attr = None
+
+        for key, value in self.selectors.items():
+
+            if isinstance(value, dict):
+                selector = value.get('selector')
+                return_type = value.get('return_type', 'string')
+                attr = value.get('attr')
+            else:
+                selector = value
+
+            output[key] = WebCrawler.get_output(
+                self._response().select(selector), return_type, attr)
+
+        return output
 
