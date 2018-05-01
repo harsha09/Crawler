9ab78b52e4db090d87dcf81fb83d4288576c0d62
Harsha Bharadwaj <harsha.bajju@gmail.com>
2018-05-01 18:21:12 +0530

    Add subselector.

--- crawler/web_crawler.py
+++ crawler/web_crawler.py
@@ -2,6 +2,7 @@ from requests import get
 from bs4 import BeautifulSoup
 import bs4.element
 from user_agent import generate_user_agent
+from urllib.parse import urlparse
 
 
 class WebCrawler(object):
@@ -36,7 +37,11 @@ class WebCrawler(object):
         self.header = kwargs.get('header', header)
     
     def _response(self, url):
-        response = get(url, headers=self.header, timeout=20, verify=False)
+        url = urlparse(url)
+        if not url.scheme:
+            url = url._replace(scheme='https')
+
+        response = get(url.geturl(), headers=self.header, timeout=20, verify=False)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'lxml')
             for script in soup(['script', 'style']):
@@ -46,22 +51,23 @@ class WebCrawler(object):
             response.raise_for_status()
 
     @classmethod
-    def _content_delimited(cls, tag, delimiter='\r\n'):
+    def _content_delimited(cls, tag, delimiter):
         if isinstance(tag, str) or isinstance(tag, bs4.element.NavigableString):
             return tag
 
         return '{}'.format(delimiter).join(
-            [i if isinstance(i, bs4.element.NavigableString) else i.text.strip() for i in tag.contents])
+            [i.strip() if isinstance(i, bs4.element.NavigableString) else i.text.strip() for i in tag.contents])
 
     @classmethod
-    def get_output(cls, markup, return_type, attr):
+    def get_output(cls, markup, return_type, attr, delimiter):
         """Returns the innerHTMLText if attr is None. If attr is passed then returns the
         text of that respective html attribute.
         For Example: if attr=href returns href of all the attributes.
         if return_type is string then string is returned by joining the list else list is returned."""
+
         if attr == 'tags':
             return markup
-        output = [dom[attr].strip().replace('\u200b', '') if attr else WebCrawler._content_delimited(dom).replace('\u200b', '') for dom in markup]
+        output = [dom[attr].strip().replace('\u200b', '') if attr else WebCrawler._content_delimited(dom, delimiter).replace('\u200b', '') for dom in markup]
 
         if return_type == 'string':
             output = ' '.join([i for i in output if i])
@@ -78,20 +84,33 @@ class WebCrawler(object):
         output = {}
         selector = ''
         return_type = 'string'
+        delimiter = '\r\n'
 
         for key, value in selectors.items():
             if isinstance(value, dict):
                 selector = value.get('selector')
                 return_type = value.get('return_type', 'string')
                 attr = value.get('attr')
+                delimiter = value.get('delimiter', '\r\n')
             else:
                 attr = None
                 selector = value
 
             if selector.find('{%') == -1:
-                output[key] = WebCrawler.get_output(
-                    self._response(url).select(selector), return_type, attr)
+                dom_selectors = self._response(url).select(selector)
+                if 'subselector' in value:
+                    dom_selectors = [i.select(value.get('subselector'))[0].text.strip() for i in dom_selectors]
+                output[key] = WebCrawler.get_output(dom_selectors, return_type, attr, delimiter)
             else:
                 output[key] = value.replace('{%', '').replace('%}', '').strip()
+
+            if 'replacer' in value and 'replacee' in value:
+                replacer = value.get('replacer')
+                replacee = value.get('replacee')
+
+                if isinstance(output[key], str):
+                    output[key] = output[key].replace(replacee, replacer).strip()
+                if isinstance(output[key], list):
+                    output[key] = [i.replace(replacee, replacer).strip() for i in output[key]]
 
         return output
